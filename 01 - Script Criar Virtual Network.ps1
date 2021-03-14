fb978d3ff60be569b70df817979ce32eb3ed8a1e
Jádson Alves <80630862+TI-Jadson@users.noreply.github.com>
2021-03-14 10:53:00 -0300

    Add files via upload

---
+++ 01 - Script Criar Virtual Network.ps1
@@ -0,0 +1,22 @@
+﻿# Variaveis
+$RG = "RG-homolog-vnet"
+$Vnetname = "Vnet-001"
+$Subnetname = "subnet-001"
+$location = "EastUS"
+$Prefixvnet = "10.0.0.0/16"
+$Prefixsubnet = "10.0.0.0/24" 
+
+# Criar Resource Group
+New-AzResourceGroup -Name $RG -Location $location 
+
+# Criar subnet 
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $Subnetname -AddressPrefix $Prefixsubnet
+
+# Criar virtual Network
+New-AzVirtualNetwork -ResourceGroupName $RG `-Location $location `-Name $Vnetname `-AddressPrefix $Prefixvnet `-Subnet $subnet
+
+# Exibir todas as virtual network criadas por nome
+Get-AzVirtualNetwork |select Name
+
+
+
